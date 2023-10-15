2229da1b926b48b80a9f81578349d5546896c908
Nepochtov Dmitry <dimendroider@gmail.com>
2023-10-16 01:32:59 +0300

    feat simple handlers

--- src/modules/Search/Search.tsx
+++ src/modules/Search/Search.tsx
@@ -1,18 +1,31 @@
-import React from 'react';
+import React from 'react'
+// import {ReactComponent as SearchIcon} from './search.svg'; //?
+import searchLogo from './search.svg';
 import style from './Search.module.scss';
-import {ReactComponent as SearchIcon} from './search.svg';
+
 
 type Props = {}
 
 export const Search = (props: Props) => {
+  const handlerSubmit = (e: React.FormEvent<EventTarget>) => {
+    alert('submited');
+    e.preventDefault();
+  }
+
+  const handlerInput = (e: React.FormEvent<EventTarget>) => {
+    console.log(e.target.value); //?
+  }
+
+
   return (
     <div>
-        <label htmlFor="town">Погода в городе:</label>
-      <form className={style.search_form}>
-        <input type="search" className={style.search_input} name="town" id="town" placeholder='Начните вводить город' />
-        <button className={style.search_btn} type="submit">Найти</button>
+      <label htmlFor="town">Погода в городе:</label>
+      <form className={style.search_form} onSubmit={handlerSubmit}>
+        <input type="search" className={style.search_input} onInput={handlerInput} name="town" id="town" placeholder='Начните вводить город' />
+        <button className={style.search_btn} type="submit">
+          <img src={searchLogo} />
+        </button>
       </form>
-      Search under form
     </div>
   )
 }
