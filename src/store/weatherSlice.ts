1887c658466ea39faccac419ac135888f2ef8eaa
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2023-10-16 18:21:27 +0300

    feat weatherSlice

--- src/store/weatherSlice.ts
+++ src/store/weatherSlice.ts
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { weatherRequestAsync } from './weatherAction';
 
 export interface IWeather {
   isLoading: boolean;
@@ -17,7 +18,24 @@ export const weatherSlice = createSlice({
   name: 'wether',
   initialState,
   reducers: {},
-  
+  extraReducers: (builder) => {
+    builder
+      .addCase(weatherRequestAsync.pending, (state) => {
+        state.isLoading = true;
+        state.error = '';
+        state.weather = {};
+      })
+      .addCase(weatherRequestAsync.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload.error ?? ''; // check error
+        state.weather = action.payload; // good data
+      })
+      .addCase(weatherRequestAsync.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.message ?? 'Неизвестная ошибка загрузки';
+        state.weather = {};
+      })
+  }
 });
 
 export default weatherSlice.reducer;
