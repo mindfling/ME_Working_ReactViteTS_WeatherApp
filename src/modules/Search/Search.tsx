04a4bf26b506b641fc97aec1daa16e29dfd426a5
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2023-10-16 18:28:01 +0300

    feat search use dispatch

--- src/modules/Search/Search.tsx
+++ src/modules/Search/Search.tsx
@@ -1,15 +1,21 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 // import {ReactComponent as SearchIcon} from './search.svg'; //?
 import style from './Search.module.scss';
 import MySearchIcon from './MySearchIcon';
+import { weatherRequestAsync } from '../../store/weatherAction';
 
 
 export const Search = () => {
+  const dispatch = useDispatch();
   const [searchText, setSearch] = useState('');
 
   const handlerSubmit = (e: React.FormEvent<EventTarget>) => {
     alert('submited search: ' + searchText);
     e.preventDefault();
+    dispatch(weatherRequestAsync(searchText));
+    
+    // todo hook который типизирует dispatch
   }
 
   // для управления нашим компонентом
