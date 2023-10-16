04d68d8796e919e4136f33bff76bae7cd07b414d
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2023-10-16 18:14:11 +0300

    feat weatherAction

--- src/store/weatherAction.ts
+++ src/store/weatherAction.ts
@@ -1,11 +1,16 @@
   import { createAsyncThunk } from '@reduxjs/toolkit';
+import { IWeather } from './weatherSlice';
   
   
-  const URL: string = `https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API key}&lang=ru&units=metric`;
+  const API_KEY: string = '9f19abcdbca7dfb8a16da935612e8051';
+  // 9f19abcdbca7dfb8a16da935612e8051
+  const myCity: string = 'Tambov';
+  const URL: string = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&lang=ru&units=metric`;
+  // https://openweathermap.org/api/one-call-3
   
-  export const weatherRequestAsync = createAsyncThunk(
+  export const weatherRequestAsync = createAsyncThunk<IWeather, string>(
     'weather/fetch',
-    async (city: string) => {
+    async (city) => {
       const response: Response = await fetch(``);
       if (response.ok && response.status >= 200 && response.status < 300) {
         return response.json;
