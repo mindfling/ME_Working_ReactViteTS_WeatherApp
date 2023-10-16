4387d3cc58414d1fca569321b0ed86321b7c8d3c
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2023-10-16 12:58:44 +0300

    try fixe svg types declare

--- src/svg.d.ts
+++ src/svg.d.ts
@@ -1,8 +1,5 @@
 declare module '*.svg' {
-  import * as React from 'react'
-
-  const ReactComponent: React.FunctionComponent<
-    React.ComponentProps<'svg'> & { title?: string }
-  >
-  export default ReactComponent
+  const content: React.FC<React.SVGProps<SVGElement>>
+  export default content
 }
+
