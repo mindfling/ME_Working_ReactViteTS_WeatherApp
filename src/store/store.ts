d86a7573cf9dfaf560c1c7fd29d9285067aea0ea
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2023-10-16 20:58:07 +0300

    Update store.ts

--- src/store/store.ts
+++ src/store/store.ts
@@ -12,3 +12,5 @@ export const store = configureStore({
 
 export type AppStore = typeof store;
 export type AppDispatch = AppStore['dispatch'];
+export type RootState = ReturnType<typeof rootReducer>;
+
